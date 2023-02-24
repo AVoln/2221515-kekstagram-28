7d3ce6240fd4ede5166c5c1f83bfed6fcf9908ca
A.Voln <a.voln@list.ru>
2023-02-24 17:54:01 +0300

    fix mistakes in functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,14 @@
 //проверка длины строки
-const getStrokeLength = (stroke, length) => {
-  if (stroke.length >= length) {
+const isValidLength = (string, length) => string.length >= length;
 
-    return true;
-  }
-
-  return false;
-};
-
-getStrokeLength('Привет, как дела?', 20);
+isValidLength('Привет, как дела?', 20);
 
 //проверка на палиндром
 
 const isPalindrome = (stroke) => {
   const normalizedStroke = `${stroke}`.split(' ').join('').toLowerCase();
 
-  return !!(normalizedStroke.split('').reverse().join('') === normalizedStroke);
+  return normalizedStroke.split('').reverse().join('') === normalizedStroke;
 };
 
 isPalindrome('Лёша на полке клопа нашёл');
@@ -23,22 +16,22 @@ isPalindrome('Лёша на полке клопа нашёл');
 //извлечение чисел из строки
 
 const getNumber = (value) => {
-  const array = `${value}`.split('');
+  const array = value.toString().split('');
 
   const numbers = array.filter((item) => {
-    const number = parseFloat(item);
+    const number = parseInt(item, 10);
 
-    return !!(number === 0 || number);
+    return Number.isInteger(number);
   });
 
   return numbers.join('');
 };
 
-getNumber('-ыыы 1ывфыв222фывфы 3123');
+getNumber('-ыыы 12333333333333wwwas 333355 3123');
 
 // //формирование адресов файлов(добивка)
 
-const addSymbol = (startStroke, minLength, additionalStroke) => {
+const fillStartString = ({ startStroke, minLength, additionalStroke }) => {
   const startArray = `${startStroke}`.split('');
   const additionalArray = `${additionalStroke}`.split('');
 
@@ -51,18 +44,16 @@ const addSymbol = (startStroke, minLength, additionalStroke) => {
 
     while (result.length < minLength) {
       const delta = minLength - result.length;
-      if (delta > 0) {
-        if (additionalStroke.length <= delta) {
-          result = [...additionalArray, ...result];
-        } else {
-          additionalArray.length = delta;
-          result = [...additionalArray, ...result];
-        }
+      if (additionalStroke.length <= delta) {
+        result = [...additionalArray, ...result];
+      } else {
+        additionalArray.length = delta;
+        result = [...additionalArray, ...result];
       }
     }
     return result.join('');
   }
 };
 
-addSymbol('q', 4, 'we');
+fillStartString({ startStroke: 'q', minLength: 3, additionalStroke: 'we' });
 
